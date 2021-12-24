348c86fbe3788ec28744b03d304bca180bb94b03
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2021-12-24 08:26:58 -0500

    WIP: importing

--- raw/import.jl
+++ raw/import.jl
@@ -2,7 +2,7 @@ using SQLite
 using ZipFile
 using CSV
 using DataFrames
-
+using Dates
 
 #
 #using Profile
@@ -11,184 +11,158 @@ using DataFrames
 
 # generate SQLite file
 conn = SQLite.DB("backblaze.sqlite3")
+ref_date=Date("2010-01-01")
+
+SQLite.execute(conn,
+"""
+create table if not exists serial_number(
+    val TEXT NOT NULL,
+    id  INTEGER NOT NULL
+)
+"""
+);
+
+SQLite.execute(conn,
+"""
+create table if not exists model(
+    val TEXT NOT NULL,
+    id  INTEGER NOT NULL
+)
+"""
+);
+
 
 SQLite.execute(conn, 
 """
-CREATE TABLE if not exists drive_stats (
-    date TEXT NOT NULL,
-    serial_number TEXT NOT NULL,
-    model TEXT NOT NULL,
+CREATE TABLE if not exists drive_stats(
+    day INTEGER NOT NULL,
+    serial_number_id INTEGER NOT NULL,
+    model_id INTEGER NOT NULL,
     capacity_bytes INTEGER (8) NOT NULL,
     failure INTEGER (1) NOT NULL,
+    
     smart_1_normalized INTEGER,
     smart_1_raw INTEGER,
-    smart_2_normalized INTEGER,
-    smart_2_raw INTEGER,
-    smart_3_normalized INTEGER,
-    smart_3_raw INTEGER,
-    smart_4_normalized INTEGER,
     smart_4_raw INTEGER,
-    smart_5_normalized INTEGER,
     smart_5_raw INTEGER,
-    smart_7_normalized INTEGER,
-    smart_7_raw INTEGER,
-    smart_8_normalized INTEGER,
-    smart_8_raw INTEGER,
-    smart_9_normalized INTEGER,
     smart_9_raw INTEGER,
-    smart_10_normalized INTEGER,
-    smart_10_raw INTEGER,
-    smart_11_normalized INTEGER,
-    smart_11_raw INTEGER,
-    smart_12_normalized INTEGER,
     smart_12_raw INTEGER,
-    smart_13_normalized INTEGER,
-    smart_13_raw INTEGER,
-    smart_15_normalized INTEGER,
-    smart_15_raw INTEGER,
-    smart_16_normalized INTEGER,
-    smart_16_raw INTEGER,    
-    smart_17_normalized INTEGER,
-    smart_17_raw INTEGER,
-    smart_18_normalized INTEGER,
-    smart_18_raw INTEGER,
     smart_22_normalized INTEGER,
-    smart_22_raw INTEGER,
-    smart_23_normalized INTEGER,
-    smart_23_raw INTEGER,
-	smart_24_normalized INTEGER,
-    smart_24_raw INTEGER,
-    smart_160_normalized INTEGER,
-    smart_160_raw INTEGER,        
-    smart_161_normalized INTEGER,
-    smart_161_raw INTEGER,        
-    smart_163_normalized INTEGER,
-    smart_163_raw INTEGER,        
-    smart_164_normalized INTEGER,
-    smart_164_raw INTEGER,
-    smart_165_normalized INTEGER,
-    smart_165_raw INTEGER,        
-    smart_166_normalized INTEGER,
-    smart_166_raw INTEGER,        
-    smart_167_normalized INTEGER,
-    smart_167_raw INTEGER,        
-    smart_168_normalized INTEGER,
-    smart_168_raw INTEGER,        
-    smart_169_normalized INTEGER,
-    smart_169_raw INTEGER,        
     smart_170_normalized INTEGER,
-    smart_170_raw INTEGER,        
-    smart_173_normalized INTEGER,
-    smart_173_raw INTEGER,       
-    smart_174_normalized INTEGER,
     smart_174_raw INTEGER,            
-    smart_175_normalized INTEGER,
-    smart_175_raw INTEGER,
-    smart_176_normalized INTEGER,
-    smart_176_raw INTEGER,
-    smart_177_normalized INTEGER,
-    smart_177_raw INTEGER,    
+
     smart_178_normalized INTEGER,
-    smart_178_raw INTEGER,    
-    smart_179_normalized INTEGER,
-    smart_179_raw INTEGER,    
-    smart_181_normalized INTEGER,
-    smart_181_raw INTEGER,    
-    smart_182_normalized INTEGER,
-    smart_182_raw INTEGER,    
-    smart_183_normalized INTEGER,
-    smart_183_raw INTEGER,
-    smart_184_normalized INTEGER,
-    smart_184_raw INTEGER,
-    smart_187_normalized INTEGER,
+    smart_178_raw INTEGER,
     smart_187_raw INTEGER,
-    smart_188_normalized INTEGER,
     smart_188_raw INTEGER,
-    smart_189_normalized INTEGER,
-    smart_189_raw INTEGER,
-    smart_190_normalized INTEGER,
-    smart_190_raw INTEGER,
-    smart_191_normalized INTEGER,
-    smart_191_raw INTEGER,
-    smart_192_normalized INTEGER,
     smart_192_raw INTEGER,
-    smart_193_normalized INTEGER,
     smart_193_raw INTEGER,
-    smart_194_normalized INTEGER,
     smart_194_raw INTEGER,
-    smart_195_normalized INTEGER,
-    smart_195_raw INTEGER,
-    smart_196_normalized INTEGER,
+
     smart_196_raw INTEGER,
-    smart_197_normalized INTEGER,
     smart_197_raw INTEGER,
-    smart_198_normalized INTEGER,
     smart_198_raw INTEGER,
-    smart_199_normalized INTEGER,
-    smart_199_raw INTEGER,
-    smart_200_normalized INTEGER,
-    smart_200_raw INTEGER,
-    smart_201_normalized INTEGER,
-    smart_201_raw INTEGER,
-    smart_218_normalized INTEGER,
-    smart_218_raw INTEGER,        
-    smart_220_normalized INTEGER,
-    smart_220_raw INTEGER,
-    smart_222_normalized INTEGER,
-    smart_222_raw INTEGER,
-    smart_223_normalized INTEGER,
-    smart_223_raw INTEGER,
-    smart_224_normalized INTEGER,
-    smart_224_raw INTEGER,   
-    smart_225_normalized INTEGER,
-    smart_225_raw INTEGER,
-    smart_226_normalized INTEGER,
-    smart_226_raw INTEGER,
+
     smart_231_normalized INTEGER,
-    smart_231_raw INTEGER,
     smart_232_normalized INTEGER,
-    smart_232_raw INTEGER,
+
     smart_233_normalized INTEGER,
-    smart_233_raw INTEGER,    
-    smart_235_normalized INTEGER,
-    smart_235_raw INTEGER,    
-    smart_240_normalized INTEGER,
-    smart_240_raw INTEGER,
-    smart_241_normalized INTEGER,
     smart_241_raw INTEGER,
-    smart_242_normalized INTEGER,
-    smart_242_raw INTEGER,
-    smart_250_normalized INTEGER,
-    smart_250_raw INTEGER,
-    smart_251_normalized INTEGER,
-    smart_251_raw INTEGER,
-    smart_252_normalized INTEGER,
-    smart_252_raw INTEGER,
-    smart_254_normalized INTEGER,
-    smart_254_raw INTEGER,
-    smart_255_normalized INTEGER,
-    smart_255_raw INTEGER,
-
-    smart_180_normalized INTEGER,
-    smart_180_raw INTEGER,
-    smart_202_normalized INTEGER,
-    smart_202_raw INTEGER,
-    smart_206_normalized INTEGER,
-    smart_206_raw INTEGER,
-    smart_210_normalized INTEGER,
-    smart_210_raw INTEGER,
-    smart_234_normalized INTEGER,
-    smart_234_raw INTEGER,
-    smart_245_normalized INTEGER,
-    smart_245_raw INTEGER,
-    smart_247_normalized INTEGER,
-    smart_247_raw INTEGER,
-    smart_248_normalized INTEGER,
-    smart_248_raw INTEGER
-
+    smart_242_raw INTEGER
     );
 """
-)
+);
+
+fields=["day" ,
+    "serial_number_id" ,
+    "model_id" ,
+    "capacity_bytes",
+    "failure" ,
+
+    "smart_1_normalized",
+    "smart_1_raw",
+    "smart_4_raw",
+    "smart_5_raw",
+    "smart_9_raw",
+    "smart_12_raw",
+    "smart_22_normalized",
+    "smart_170_normalized",
+    "smart_174_raw",            
+
+    "smart_178_normalized",
+    "smart_178_raw",
+    "smart_187_raw",
+    "smart_188_raw",
+    "smart_192_raw",
+    "smart_193_raw",
+    "smart_194_raw",
+
+    "smart_196_raw",
+    "smart_197_raw",
+    "smart_198_raw",
+
+    "smart_231_normalized",
+    "smart_232_normalized",
+
+    "smart_233_normalized",
+    "smart_241_raw",
+    "smart_242_raw",
+]
+
+serial_number=Dict()
+model=Dict()
+serial_id=0
+model_id=0
+
+function serial_to_id(x)
+    if haskey(serial_number,x)
+        return get(serial_number,x,serial_id)
+    else
+        global serial_id=serial_id+1
+        return get!(serial_number,x,serial_id)
+    end
+end
+
+function model_to_id(x)
+    if haskey(model,x)
+        return get(model,x,model_id)
+    else
+        global model_id=model_id+1
+        return get!(model,x,model_id)
+    end
+end
+
+function dict_to_table(d,t)
+    SQLite.transaction(conn) do 
+        SQLite.execute(conn,"delete from $(t)");
+        stmt = SQLite.Stmt(conn,"insert into $(t)(val,id) values (?,?)")
+
+        for r in d
+            SQLite.execute(stmt, [r[1], r[2]] )
+        end
+    end
+end
+
+function normalize!(df)
+    for f in setdiff(fields,names(df))
+        insertcols!(df, f=>nothing)
+    end
+    # enforce data integrity
+    #
+    # date TEXT NOT NULL,
+    # serial_number TEXT NOT NULL,
+    # model TEXT NOT NULL,
+    # capacity_bytes INTEGER (8) NOT NULL,
+    # failure INTEGER (1) NOT NULL,
+    #subset(df,:date=>ByRow())
+    dropmissing!(df, [:date,:serial_number,:model,:capacity_bytes,:failure])
+    transform!(df,:date=>ByRow(x -> (x-ref_date).value ) => :day )
+    transform!(df,:serial_number=>ByRow(serial_to_id) => :serial_number_id )
+    transform!(df,:model=>ByRow(model_to_id) => :model_id )
+
+    # select only interesting fields
+    return df[!,fields]    
+end
 
 function process_table(t)
     println(t.name)
@@ -211,10 +185,10 @@ function process_table(t)
     #    :failure => ByRow(!isnothing),
     #    )
 
-    dropmissing!(df, [:date,:serial_number,:model,:capacity_bytes,:failure])
+    df = normalize!(df)
 
-    n = join( names(df),"," )
-    q = join( repeat(["?"],ncol(df)), "," )
+    n = join( fields,"," )
+    q = join( repeat(["?"], length(fields)), "," )
 
     SQLite.transaction(conn) do 
         stmt = SQLite.Stmt(conn,"insert into drive_stats($(n)) values ($(q))")
@@ -223,12 +197,14 @@ function process_table(t)
             SQLite.execute(stmt, [ df[r,ii] for ii in 1:ncol(df) ] )
         end
     end
-    df = nothing
     # HACK?
+    df = nothing
+
+    dict_to_table(serial_number, "serial_number")
+    dict_to_table(model, "model")
 end
 
 function load_data(d)
-
     for d in filter(x->startswith(x,"data_")&&endswith(x,".zip"),readdir(d))
         println(d)
         z=nothing
