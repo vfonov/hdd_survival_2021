ccf1c85bf5734b612cc9c7dffb28cc6791a3f605
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2021-12-25 14:45:22 -0500

    WIP: studying survival

--- raw/stats.jl
+++ raw/stats.jl
@@ -11,10 +11,12 @@ model_id = (DBInterface.execute(conn,"select id from model where val=?",[model])
 
 drive_surv = DBInterface.execute(conn,"select age,failure from drive_surv where model_id=?",[model_id])|>DataFrame
 
+transform!(drive_surv,:failure => categorical -> :FAIL)
 
-p = plot(drive_surv, x=:age, color=:failure, Theme(alphas=[0.6]),
-    Stat.density(bandwidth=0.5), Geom.polygon(fill=true, preserve_order=true),
-    Guide.colorkey(title="", pos=[2.5,0.6]), Guide.title("Kernel PDF")
+# Geom.polygon(fill=true, preserve_order=true)
+
+p = plot(drive_surv, x=:age, color=:FAIL, Theme(alphas=[0.8]),
+    Stat.density(bandwidth=0.5)
 )
 
 p |> SVG("ages_$(model).svg",6inch,4inch)
