84672843fcc878b0d213cb74e11d4e25c50e43e6
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2022-01-15 20:00:39 -0500

    WIP: non-paramtric model shows limitations

--- Backblaze.jl
+++ Backblaze.jl
@@ -15,8 +15,8 @@ end
 function backblaze_drive_surv(model)
     @info(conn)
 
-    model_id = (DBInterface.execute(conn,"select id from model where val=?",[model]) |> DataFrame ).id[1]
-    drive_surv = DBInterface.execute(conn,"select age,failure from drive_surv where model_id=?",[model_id])|>DataFrame
+    model_id  = (DBInterface.execute(conn,"select id from model where val=?",[model]) |> DataFrame ).id[1]
+    drive_surv = DBInterface.execute(conn,"select age,failure from drive_surv where model_id=?",[model_id]) |> DataFrame
 
     transform!(drive_surv,:failure => ByRow(x-> x==1) => :FAIL)
 end
--- hdd_survival_bootstrap.jl
+++ hdd_survival_bootstrap.jl
@@ -2,7 +2,7 @@ using DataFrames
 using Distributions
 using Logging
 using Statistics
-using Bootstrap
+#using Bootstrap
 using ProgressBars
 
 using Gadfly
@@ -21,82 +21,93 @@ model = "ST12000NM001G"
 drive_surv_3 = backblaze_drive_surv(model)
 
 drive_surv = vcat(drive_surv_1,drive_surv_2,drive_surv_3)
-
 model = "ST12000"
+
+@info "Sorting to speedup ?"
+sort!(drive_surv,[:age])
+
 
 failed_drive_surv = subset(drive_surv,:failure => ByRow(x->x==1))
 censored_drive_surv = subset(drive_surv,:failure => ByRow(x->x==0))
 
-global_max_age = maximum(drive_surv.age)
+global_max_age = drive_surv.age[end]# should be sorted
 
-@info "Maximum drive age:$(global_max_age/365)y N=$(nrow(drive_surv))"
+@info "Maximum drive age:$(global_max_age/365)y N=$(nrow(drive_surv)) Nfail=$(nrow(failed_drive_surv))"
 @info "Median failed age:$(median(failed_drive_surv.age)/365)y"
 @info "Median censored age:$(median(censored_drive_surv.age)/365)y"
 
 
+mutable struct check_result
+    fail::Bool
+    fail_day::Int64
+    n_rebuild::Int64
+
+    function check_result(fail=false,fail_day=0,n_rebuild=0) 
+        new(fail,fail_day,n_rebuild)
+    end
+end
+
+
+
 # return (n_fail,n_rebuild)
-function check_fail(s, n_raid::Integer, time_window::Integer)::Vector{Integer}
+function check_fail(s::DataFrame, n_raid::Int64, time_window::Int64)::check_result
     # return (failed,age,n_rebuid)
     censored::Vector{Int64} = subset(s,:failure => ByRow(x-> x == 0)).age
-    min_censored_age::Integer = global_max_age
+    min_censored_age::Int64 = global_max_age
 
     if length(censored)>0
-        min_censored_age = minimum(censored)
+        min_censored_age = censored[1]
     end
+    result = check_result()
 
-    failed::Vector{Int64} = subset(s,:failure => ByRow(x-> x==1 ),:age => ByRow(x-> x<= min_censored_age)).age
+    failed::Vector{Int64} = subset(s,:failure => ByRow(x->x==1), :age => ByRow(x-> x<= min_censored_age)).age
     n_rebuild::Integer = length(failed)
 
     if n_rebuild == 0
-        return [0, min_censored_age, 0]
+        return check_result(false, min_censored_age, 0)
     elseif  n_rebuild <= n_raid
-        return [0, min_censored_age, n_rebuild]
+        return check_result(false, min_censored_age, n_rebuild)
     else
-        sort!(failed)
-        if failed[end] > min_censored_age # we don't actually know, because non-failed drives got censored earlier
-            return [0, min_censored_age, 0]
-        else
-            fail::Integer = 0
-            fail_day::Integer = failed[1]
-            
-            for n=1:(n_rebuild-n_raid)
-                if (failed[n+n_raid]-failed[n]) <= time_window
-                    fail = 1
-                    fail_day = failed[n+n_raid]
-                    break
-                end
+        fail::Bool = 0
+        fail_day = failed[1]
+        
+        for n=1:(n_rebuild-n_raid)
+            if (failed[n+n_raid]-failed[n]) <= time_window
+                fail = 1
+                fail_day = failed[n+n_raid]
+                break
             end
-            return [fail, fail_day, n_rebuild]
         end
+        return check_result(fail, fail_day, n_rebuild)
     end
 end
 
 
 function simulate_raid(drive_surv, n_samples::Integer, n_pools::Integer, n_drives::Integer, n_raid::Integer, time_window::Integer) 
-    results = DataFrame(failed=zeros(Int32, n_samples), age=zeros(Int32, n_samples), n_rebuild=zeros(Int32, n_samples))
+    results = DataFrame(failed=zeros(Bool, n_samples), age=zeros(Int64, n_samples), n_rebuild=zeros(Int64, n_samples))
 
     Threads.@threads for i in ProgressBar(1:n_samples)
-        pool_res::Vector{Integer}=[0,0,0]
+        pool_res=check_result()
         for p = 1:n_pools
-            ss = sample(1:nrow(drive_surv), n_drives, replace=false)
+            ss = sample(1:nrow(drive_surv), n_drives; replace=true,ordered=true)
             fl = check_fail(drive_surv[ss,:], n_raid, time_window)
             if p == 1
-                pool_res .= fl
-            elseif fl[1]==1 && ((pool_res[1] == 1 && fl[2]<pool_res[2]) || pool_res[1]==0)# failed
-                    pool_res .= fl
+                pool_res = fl
+            elseif fl.fail && ((pool_res.fail && fl.fail_day<pool_res.fail_day) || !pool_res.fail)
+                    pool_res = fl
             else
-                pool_res[3] += fl[3]
-                if pool_res[2] > fl[2]
-                    pool_res[2] = fl[2]
+                pool_res.n_rebuild += fl.n_rebuild
+                if pool_res.fail_day > fl.fail_day
+                    pool_res.fail_day = fl.fail_day
                 end
             end
         end
 
-        results[i,:] = pool_res
+        results[i,:] = (pool_res.fail, pool_res.fail_day, pool_res.n_rebuild)
     end
 
 
-    failed = subset(results,:failed => ByRow(x->x==1))
+    failed = subset(results,:failed)
     # get the minimum censored time
     mean_time_to_failure = mean(failed.age)
     pct_failed     = mean(results.failed)
@@ -109,10 +120,10 @@ end
 # simulating on real example 
 # how to split 40 drives:
 
-simulations = DataFrame(
-    n_drives= [19,13,10],
-    n_raid  =  [3, 3, 2],
-    n_pools =  [2, 3, 4]
+simulations  = DataFrame(
+    n_drives =  [9, 9, 9, 9],
+    n_raid   =  [2, 2, 2, 2],
+    n_pools  =  [1, 2, 3, 4]
 )
 
 
@@ -127,7 +138,7 @@ simulations_results = DataFrame(
 )
 
 time_window = 10       # days
-n_samples = 10_000_000 # number of simulations
+n_samples = 100_000_000 # number of simulations
 
 
 all_fits = DataFrame(
@@ -168,7 +179,7 @@ end
 
 for i=1:nrow(simulations)
 
-    (pct_failed,pct_rebuild,median_rebuild,mean_time_to_failure,results)=
+    (pct_failed,pct_rebuild,median_rebuild,mean_time_to_failure, results)=
         simulate_raid(drive_surv, n_samples,simulations.n_pools[i], simulations.n_drives[i], simulations.n_raid[i], time_window)
 
     push!(simulations_results, (simulations.n_pools[i], simulations.n_drives[i], simulations.n_raid[i],pct_failed,pct_rebuild,median_rebuild,mean_time_to_failure))
