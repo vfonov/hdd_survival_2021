a1405ba882ee81a4a89b263949426f75670a5e07
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2022-06-01 11:32:04 -0400

    Added weibull example

--- surv_tutorial.jl
+++ surv_tutorial.jl
@@ -1,9 +1,7 @@
 # Import Turing and Distributions.
 using Turing, Distributions
-#using Zygote
 using Optim
 using StatsBase
-# Import RDatasets.
 using RDatasets
 using Survival
 import Cairo, Fontconfig
@@ -12,7 +10,7 @@ import Cairo, Fontconfig
 using MCMCChains, Plots, StatsPlots
 
 # Functionality for splitting and normalizing the data
-using MLDataUtils: shuffleobs, stratifiedobs, rescale!
+#using MLUtils: shuffleobs, stratifiedobs, rescale!
 
 using Logging
 
@@ -21,46 +19,52 @@ using Random
 Random.seed!(0);
 
 # Turn off progress monitor.
-Turing.setprogress!(false)
-#Turing.setadbackend(:zygote)
+#Turing.setprogress!(false)
 
 # Import the "Default" dataset.
 data = RDatasets.dataset("HSAUR", "mastectomy");
 
 # Show the first six rows of the dataset.
 # Convert "Default" and "Student" to numeric values.
-data[!,:MetastizedNum] = [r.Metastized == "yes" ? 1.0 : 0.0 for r in eachrow(data)]
+data[!,:MetastizedNum] = [r.Metastized == "yes" ? 2 : 1 for r in eachrow(data)]
 data[!,:LogTime] = log.(data.Time)
-μ, σ = rescale!(data[!,:LogTime], obsdim=1)
+
+#μ, σ = rescale!(data[!,:LogTime], obsdim=1)
+log_time_scale = 1.0/ maximum(data[!,:LogTime])
+data[!,:LogTimeS] = data[!,:LogTime]*log_time_scale
+
 # Delete the old columns which say "Yes" and "No".
 #select!(data, Not([:Metastized,:Event]))
 
 # Show the first six rows of our edited dataset.
 @info first(data, 6)
 
-features = [:MetastizedNum]
-target   = :Time
+# build non-parametric K-M estimator
+fit_km_1 = fit(KaplanMeier, data.Time[data.MetastizedNum.==1], data.Event[data.MetastizedNum.==1])
+#fit_km_1_c = reinterpret(reshape, Float64, confint(fit_km_1))
 
-# Turing requires data in matrix form, not dataframe
-#train = Matrix(data[:, features])
-#train_label = data[:, target]
+fit_km_2 = fit(KaplanMeier, data.Time[data.MetastizedNum.==2], data.Event[data.MetastizedNum.==2])
+#fit_km_2_c = reinterpret(reshape, Float64, confint(fit_km_2))
 
-#@info train
-#@info train_label
 
-@model function survival_regression(log_time, factor, event)
-    #σ = 5
-    σ ~ truncated(Normal(0, 100), 0, Inf)
-    σ2 ~ truncated(Normal(0, 100), 0, Inf)
+plot(fit_km_1.times, fit_km_1.survival)#fit_km_c[1,:], ymax=fit_km_c[2,:]
+plot!(fit_km_2.times, fit_km_2.survival)
+#plot!(fit_km.times, pred_weib_1)
+savefig("surv_km.png")
 
-    intercept  ~ Normal(0, sqrt(σ))
-    metastized ~ Normal(0, sqrt(σ))
 
-    for i = eachindex(log_time)
+# log-normal model
+@model function survival_lognormal(log_time, MetastizedNum, event)
+    # priors
+    #σ ~ LogNormal(0,1)
+    σ ~ Exponential(2)
+    metastized ~ MvNormal([0, 0], 1.0)
 
-        θ = intercept + metastized*factor[i]
+    # fitting data
+    for i in eachindex(log_time)
+        θ = metastized[MetastizedNum[i]]
 
-        dist = Normal(θ, sqrt(σ2))
+        dist = Normal(θ, σ)
         if event[i] # not-censored
             log_time[i] ~ dist
         else # censored
@@ -68,45 +72,79 @@ target   = :Time
         end
         
     end
+    return log_time
 end;
 
-#@info "train:$(size(train)) train_label:$(size(train_label))"
+model = survival_lognormal(data.LogTimeS, data.MetastizedNum, data.Event) # , 1.0,0.1
 
-# Sample using HMC.
-# convert(Array{Float64,1}, data.Time)
-model = survival_regression(data.LogTime, data.MetastizedNum, data.Event) # , 1.0,0.1
-chain = sample(model, NUTS(0.65), MCMCThreads(), 2_000, 4)
-summaries, quantiles = describe(chain);
+function simulate_survival_lognorm(chain::Chains, sym=Symbol("metastized[1]"))
+    # sample from chain
 
-@info summaries
+    # generate survival curves
+    sim_range = LinRange(minimum(data.LogTimeS), maximum(data.LogTimeS)*1.1, 20)
 
-plot(chain)
-savefig("surv_fig1.png")
+    fits = hcat( ( ccdf( Normal(θ, σ),sim_range ) for (θ, σ) in zip(chain[sym],chain[Symbol("σ")]) )... )
 
-# calculate mean predictions
-intercept_exp  = mean(chain[:intercept])
-metastized_exp = mean(chain[:metastized])
-@info "mean intercept:$(intercept_exp) $(size(chain[:intercept])) mean metastized:$(metastized_exp)"
+    return exp.(sim_range ./ log_time_scale), fits
+end
 
-# Generate a MLE estimate.
+function simulate_survival_lognorm(chain::ChainDataFrame,sym=Symbol("metastized[1]"))
+    # sample from chain summary 
+
+    # generate survival curves
+    sim_range = LinRange(minimum(data.LogTimeS), maximum(data.LogTimeS)*1.1, 20)
+
+    fits = hcat( ( ccdf( Normal(θ, σ),sim_range ) for (θ, σ) in zip(chain[sym,:mean],chain[Symbol("σ"),:mean]) )... )
+
+    return exp.(sim_range ./ log_time_scale), fits
+end
+
+begin
+    sim_time,sim_fit=simulate_survival_lognorm(sample(model, Prior(), 1000))
+
+
+    plot( sim_time, sim_fit, xlabel="Time",ylabel="Survival",title="Prior distribution",color=:gray,legend = false,alpha=0.1)
+    savefig("surv_priors_lognorm.png")
+end
+
+begin
+    # Sample using HMC.
+    chain = sample(model, NUTS(0.65), MCMCThreads(), 1_000, 4)
+    summaries, quantiles = describe(chain);
+    show(IOContext(stdout, :limit => false), "text/plain",summaries)
+
+    plot(chain)
+    savefig("surv_chain1.png")
+end
+
+begin
+
+    sim_time,sim_fit=simulate_survival(sample(resetrange(chain), 1000))
+
+    sim_time_mean1,sim_fit_mean1=simulate_survival_lognorm(mean(chain),Symbol("metastized[1]"))
+    sim_time_mean2,sim_fit_mean2=simulate_survival_lognorm(mean(chain),Symbol("metastized[2]"))
+
+    plot( sim_time, sim_fit, xlabel="Time",ylabel="Survival",title="Posterior distribution",color=:gray,legend = false,alpha=0.1)
+    plot!(sim_time_mean1,sim_fit_mean1,color=:blue )
+    plot!(sim_time_mean2,sim_fit_mean2,color=:red )
+
+    savefig("surv_posterior_lognorm.png")
+end
+
 mle_estimate = optimize(model, MLE())
 @info "MLE estimate"
 @info mle_estimate.values
-#@info coeftable(mle_estimate)
 
-
-# using weibull distribution
-@model function survival_regression_weibull(log_time, factor, event)
-    σ1 ~ truncated(Normal(0, 100), 0, Inf)
-    σ2 ~ truncated(Normal(0, 100), 0, Inf)
-
-    scale      ~ truncated(Normal(0, 10), 0.0, Inf) # HACK
-    intercept  ~ Normal(0, sqrt(σ1))
-    metastized ~ Normal(0, sqrt(σ2))
+# using weibull distribution, with different scale
+@model function survival_regression_weibull(log_time, MetastizedNum, event)
+    # priors
+    metastized ~ MvNormal([0, 0], 1.0)
+    metastized_scale ~ MvLogNormal([0, 0], 1.0)
 
     for i = eachindex(log_time)
-        # accceleration facctor
-        θ = intercept + metastized*factor[i]
+        # accceleration factor
+        θ     = metastized[MetastizedNum[i]]
+        scale = metastized_scale[MetastizedNum[i]]
 
         time_dist = Gumbel(θ, scale)
         if event[i] # not-censored
@@ -118,41 +156,25 @@ mle_estimate = optimize(model, MLE())
     end
 end;
 
-
-model2 = survival_regression_weibull(data.LogTime, data.MetastizedNum, data.Event) # , 1.0,0.1
-chain2 = sample(model2, NUTS(0.65), MCMCThreads(), 2_000, 4)
+model2 = survival_regression_weibull(data.LogTimeS, data.MetastizedNum, data.Event) # , 1.0,0.1
+chain2 = sample(model2, NUTS(0.65), MCMCThreads(), 1_000, 4)
 summaries2, quantiles2 = describe(chain2);
 
 @info summaries2
 
 plot(chain2)
-savefig("surv_fig2.png")
+savefig("surv_chain2.png")
 
-# calculate mean predictions
-intercept = mean(chain2[:intercept])
-metastized = mean(chain2[:metastized])
-scale = mean(chain2[:scale])
-@info "mean intercept:$(intercept) mean metastized:$(metastized) mean scale:$scale"
+@info mean(chain[Symbol("metastized[1]")]),mean(chain[Symbol("metastized[2]")])
+@info mean(chain[Symbol("metastized_scale[1]")]),mean(chain[Symbol("metastized_scale[2]")])
 
 mle_estimate2 = optimize(model2, MLE())
 @info "MLE estimate"
 @info mle_estimate2.values
 @info mle_estimate2
-#c=coeftable(mle_estimate2)
-#@info describe(mle_estimate2)
-
-# build K-M estimator
-fit_km = fit(KaplanMeier, data.Time, data.Event)
-fit_km_c = reinterpret(reshape, Float64, confint(fit_km))
-log_times = log.(fit_km.times)
-rescale!(log_times, μ, σ)
-
-fit_weibull=exp.( - exp.(intercept_exp .+ log_times ) )
 
 # perform posterior and prior predictive checks on the models
 
-plot(fit_km.times, fit_km.survival)
-plot!(fit_km.times, pred_weib_1)
 savefig("surv_fig3.png")
 
 # p1 = plot(x=fit_km.times, y=fit_km.survival, ymin=fit_km_c[1,:], ymax=fit_km_c[2,:],color="green",
