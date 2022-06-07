3d80ebebcc577ad1ba3f9b41536727a173b3e0f2
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2022-06-07 15:46:37 -0400

    Added log-logistic model

--- hdd_surv_bayes_many2.jl
+++ hdd_surv_bayes_many2.jl
@@ -69,15 +69,15 @@ plot(km_stats, x=:times, y=:surv, color=:model,
 # Show the first six rows of our edited dataset.
 @info first(data, 6)
 
-# using weibull distribution
-@model function survival_weibul(log_time::Vector{Float64}, event::Vector{Bool}, hdd_model::Vector{Int64})
+# using loglogistic distribution
+@model function survival_loglogistic(log_time::Vector{Float64}, event::Vector{Bool}, hdd_model::Vector{Int64})
     # priors 
     μ ~ MvNormal( zeros(n_model), 1.0)
 	# prior scale
 	θ ~ LogNormal(0.0, 1.0) # truncated(Normal(0.0,5.0),0.0,Inf) MvLogNormal ?
     # fitting data
     for i in eachindex(log_time)
-        dist = Gumbel(μ[hdd_model[i]], θ)
+        dist = Logistic(μ[hdd_model[i]], θ)
         if event[i] # not-censored
             log_time[i] ~ dist
         else # censored
@@ -86,9 +86,9 @@ plot(km_stats, x=:times, y=:surv, color=:model,
     end
 end;
 
-surv_model_weibull=survival_weibul( data.n_log_age, data.failure, levelcode.(data.model) ) 
+surv_model_loglogistic=survival_loglogistic( data.n_log_age, data.failure, levelcode.(data.model) ) 
 
-function simulate_survival_weibul(chain::Chains; sym="μ[1]",p=5)
+function simulate_survival_loglogistic(chain::Chains; sym="μ[1]",p=5)
     # sample from chain, times are in years
     # extract percntiles : p , 50, 100-p
 
@@ -96,7 +96,7 @@ function simulate_survival_weibul(chain::Chains; sym="μ[1]",p=5)
     sim_range = LinRange(n_log_age_range[1], n_log_age_range[2], 100)
 
     sims = hcat( 
-        (ccdf( Gumbel(μ, θ), sim_range )  for (μ, θ) in zip(chain[sym], chain[Symbol("θ")]) )...
+        (ccdf( Logistic(μ, θ), sim_range )  for (μ, θ) in zip(chain[sym], chain[Symbol("θ")]) )...
     )
 
     # extract percentile
@@ -111,13 +111,13 @@ function simulate_survival_weibul(chain::Chains; sym="μ[1]",p=5)
                 )
 end;
 
-function simulate_survival_weibul(chain::ChainDataFrame; sym="μ[1]")
+function simulate_survival_loglogistic(chain::ChainDataFrame; sym="μ[1]")
     # sample from chain summary # TODO: fix this to not epect multiple sims?
 
     # generate survival curves
     sim_range = LinRange(n_log_age_range[1], n_log_age_range[2], 100)
 
-    vcat( ( DataFrame(  surv=ccdf( Gumbel(μ, θ), sim_range ), 
+    vcat( ( DataFrame(  surv=ccdf( Logistic(μ, θ), sim_range ), 
                         times=exp.(sim_range .* scale_log_age .+ mean_log_age) ./ 365,
                         sim=fill(s,length(sim_range)) )
                         for (s,(μ, θ)) in enumerate(zip(chain[sym,:mean], chain[Symbol("θ"),:mean])) )... )
@@ -125,17 +125,17 @@ end;
 
 # simulate priors
 @info "Building prior sample"
-sim_weibul_prior = simulate_survival_weibul(sample(surv_model_weibull, Prior(), 1000));
+sim_loglogistic_prior = simulate_survival_loglogistic(sample(surv_model_loglogistic, Prior(), 1000));
 
 # plot prior distribution
-plot( sim_weibul_prior, x=:times, y=:surv, ymin=:surv_p1,ymax=:surv_p2, Geom.line, Geom.ribbon, alpha=[0.6],
+plot( sim_loglogistic_prior, x=:times, y=:surv, ymin=:surv_p1,ymax=:surv_p2, Geom.line, Geom.ribbon, alpha=[0.6],
         Theme(background_color="white")) |>  # ,default_color=RGBA(0.1,0.1,0.1,0.1)
-        SVG("hdd_multi_weibul_prior.svg", 15cm, 15cm)
+        SVG("hdd_multi_loglogistic_prior.svg", 15cm, 15cm)
 
 @info "Running sampler"
-chain_weibul = sample(surv_model_weibull, NUTS(0.65), MCMCThreads(), 1_000, 4)
-#summaries2, quantiles2 = describe(chain_weibul);
-#df = DataFrame(chain_weibul)
+chain_loglogistic = sample(surv_model_loglogistic, NUTS(0.65), MCMCThreads(), 1_000, 4)
+#summaries2, quantiles2 = describe(chain_loglogistic);
+#df = DataFrame(chain_loglogistic)
 #df[!, :chain] = categorical(df.chain)
 
 # plot(first_chain, ygroup=:parameter, x=:iteration, y=:value, color=:chain,
@@ -143,23 +143,23 @@ chain_weibul = sample(surv_model_weibull, NUTS(0.65), MCMCThreads(), 1_000, 4)
 
 # plot(df, x=:A, color=:chain, 
 #     Geom.density, Guide.ylabel("Density"),Theme(background_color="white")) |> 
-#     SVG("hdd_multi_surv_chain_weibul.svg", 15cm, 15cm)
+#     SVG("hdd_multi_surv_chain_loglogistic.svg", 15cm, 15cm)
 
 # simulate distributions
-sim_weibul_post=vcat( ( 
-    insertcols!( simulate_survival_weibul(sample(resetrange(chain_weibul), 1000), sym="μ[$i]"), :model=> levels(data.model)[i])
+sim_loglogistic_post=vcat( ( 
+    insertcols!( simulate_survival_loglogistic(sample(resetrange(chain_loglogistic), 1000), sym="μ[$i]"), :model=> levels(data.model)[i])
     for i in 1:n_model)...)
 
-# sim_weibul_post_mean = vcat( ( 
-#     insertcols!( simulate_survival_weibul(mean(chain_weibul),sym="μ[$i]"), :model=> levels(data.model)[i]) 
+# sim_loglogistic_post_mean = vcat( ( 
+#     insertcols!( simulate_survival_loglogistic(mean(chain_loglogistic),sym="μ[$i]"), :model=> levels(data.model)[i]) 
 #         for i in 1:n_model)...)
 
-l1=layer(sim_weibul_post, 
+l1=layer(sim_loglogistic_post, 
          x=:times, y=:surv, 
          Geom.line, 
          color=:model)
 
-l2=layer(sim_weibul_post, 
+l2=layer(sim_loglogistic_post, 
          x=:times,
          ymin=:surv_p1, ymax=:surv_p2, 
          Geom.ribbon,color=:model)
@@ -171,4 +171,4 @@ l3=layer(km_stats, x=:times, y=:surv,
 plot( l1, l2, l3,
     Guide.colorkey(title="HDD model"),
     Theme(background_color="white",key_position=:inside,alphas=[0.6]) ) |>
-    SVG("hdd_multi_weibul_posterior.svg", 15cm, 15cm)
+    SVG("hdd_multi_loglogistic_posterior.svg", 15cm, 15cm)
