e72099718d28f1555cbe56effc242c008e31f186
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2022-07-05 09:25:27 -0400

    WIP: refactoring

--- hdd_surv_bayes_many_mix.jl
+++ hdd_surv_bayes_many_mix.jl
@@ -26,8 +26,7 @@ Turing.setprogress!(true)
 include("Backblaze.jl")
 using .Backblaze
 
-const n_model = 1
-const n_comp = 2
+const n_model = 3
 
 models = first(model_stats(),n_model)
 
@@ -79,28 +78,33 @@ plot(km_stats, x=:times, y=:surv, color=:model,
     σ2_bar=1.0
 
     # distribution weights
-    w1 ~  Normal(0,1 )
+    # always the same, otherwise model will be unedtifiable
+    const w1 = 0.0
     w2 ~  Normal(0,1 )
+    w3 ~  Normal(0,1 )
 
-    # multinomial logistic regression
-    ww = softmax([0.0,w1,w2])
 
     μ1 ~ filldist( Normal(μ_bar, σ_bar), n_model)
     μ2 ~ filldist( Normal(μ_bar, σ_bar), n_model)
     μ3 ~ filldist( Normal(μ_bar, σ_bar), n_model)
-	# prior scale , 
-    # θ1 - between 0 and 1 - failure accelerates with time 
-	θ1 ~ filldist( Beta( 1.0,1.0),n_model) 
+
+	# prior scale ,
+    # first component is always constant hazard (exponential fit) 
+    const θ1 = 1.0 
+    # θ2 - between 0 and 1 - failure accelerates with time 
+	θ2 ~ filldist( Beta( 1.0,1.0), n_model) 
     # θ2 - above 1.0 - failure slows down with time
-	θ2 ~ filldist( LogNormal(θ_bar, σ2_bar)+1.0, n_model) 
-    # third component is always constant hazard (exponential fit) 
-	θ3 = 1.0
+	θ3 ~ filldist( LogNormal(θ_bar, σ2_bar)+1.0, n_model) 
+
+    # multinomial logistic regression
+    ww = softmax([w1,w2,w3])
+
     # fitting data
     for i in eachindex(log_time)
         dist = MixtureModel(Gumbel[
-            Gumbel(μ1[hdd_model[i]],θ1[hdd_model[i]]),
+            Gumbel(μ1[hdd_model[i]],θ1),
             Gumbel(μ2[hdd_model[i]],θ2[hdd_model[i]]),
-            Gumbel(μ3[hdd_model[i]],θ3 ) ],ww)
+            Gumbel(μ3[hdd_model[i]],θ3[hdd_model[i]])],ww)
 
         if event[i] # not-censored
             log_time[i] ~ dist
@@ -112,12 +116,15 @@ end;
 
 surv_model_mix=survival_mix( data.n_log_age, data.failure, levelcode.(data.model) ) 
 
-function simulate_survival_mix(chain::Chains; μ1="μ1[1]",θ1="θ1[1]",μ2="μ2[1]",θ2="θ2[1]",μ3="μ3[1]",p=5)
+function simulate_survival_mix(chain::Chains; 
+    μ1="μ1[1]",θ2="θ2[1]",μ2="μ2[1]",θ3="θ3[1]",μ3="μ3[1]",p=5)
     # sample from chain, times are in years
     # extract percentiles : p , 50, 100-p
+    const w1 = 0.0
+    const θ1 = 1.0 
 
-    # generate survival curves
-    sim_range = LinRange(n_log_age_range[1], n_log_age_range[2], 100)
+    # generate survival curves, with fixed number of steps
+    sim_range = LinRange( n_log_age_range[1], n_log_age_range[2], 100)
 
     # TODO: change to the mixture of two gumbel
     sims = hcat( 
@@ -125,21 +132,22 @@ function simulate_survival_mix(chain::Chains; μ1="μ1[1]",θ1="θ1[1]",μ2="μ2
             MixtureModel(Gumbel[
                 Gumbel(μ1,θ1),
                 Gumbel(μ2,θ2),
-                Gumbel(μ3,1.0)
-                ],softmax([0.0,w1,w2]))
-            , sim_range )  for (μ1, θ1,μ2,θ2,μ3,w1,w2) in 
-                        zip(chain[μ1], chain[θ1], chain[μ2], chain[θ2],chain[μ3],
-                            chain["w1"],chain["w2"]) )...
+                Gumbel(μ3,θ3)
+                ],softmax([w1,w2,w3]))
+            , sim_range )  for (μ1, θ2,μ2,θ3,μ3,w2,w3) in 
+                        zip(chain[μ1], chain[θ2], chain[μ2], chain[θ3],chain[μ3],
+                            chain["w2"],chain["w3"]) )...
     )
 
-    # extract percentile
+    # extract percentiles
     p1 = percentile.(splitdims(sims, 1), p)
     pm = percentile.(splitdims(sims, 1), 50)
     p2 = percentile.(splitdims(sims, 1), 100-p)
 
+    # output data frame with simulation results
     DataFrame(  surv_p1=p1,
                 surv=pm,
-                surv_p2=p2, 
+                surv_p2=p2,
                 times=exp.(sim_range .* scale_log_age .+ mean_log_age)./365 
                 )
 end;
@@ -159,6 +167,7 @@ summaries, quantiles = describe(chain_mix);
 @info "Summaries:"
 show(IOContext(stdout, :limit => false), "text/plain",summaries)
 show(IOContext(stdout, :limit => false), "text/plain",quantiles)
+
 #df = DataFrame(chain_mix)
 #df[!, :chain] = categorical(df.chain)
 
@@ -172,7 +181,7 @@ show(IOContext(stdout, :limit => false), "text/plain",quantiles)
 # simulate distributions
 sim_mix_post=vcat( ( 
     insertcols!( simulate_survival_mix(sample(resetrange(chain_mix), 1000), 
-                μ1="μ1[$i]",θ1="θ1[$i]", μ2="μ2[$i]",θ2="θ2[$i]", μ3="μ3[$i]"), :model=> levels(data.model)[i])
+                μ1="μ1[$i]",θ2="θ2[$i]", μ2="μ2[$i]",θ3="θ3[$i]", μ3="μ3[$i]"), :model=> levels(data.model)[i])
     for i in 1:n_model)...)
 
 # sim_mix_post_mean = vcat( ( 
